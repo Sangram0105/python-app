7db7e7c6ec818e8ca9ca9a82d374071a26b4c466
Sangram Dattatray Sankpal <115467468+Sangram0105@users.noreply.github.com>
2025-01-21 22:15:32 +0530

    Add files via upload
    The first commit

---
+++ web-app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route('/error')
+def trigger_error():
+    app.logger.error("An error occurred.")
+    return jsonify({"error": "Bad Request"}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
